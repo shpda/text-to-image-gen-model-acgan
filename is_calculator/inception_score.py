fixed several sampling and score calculation bug

is_calculator/inception_score.py
@@ -69,7 +69,8 @@ def inception_score(data_loader, cuda=True, batch_size=32, resize=False, splits=
     return np.mean(split_scores), np.std(split_scores)
 
 def load_dataset(batch_size):
-    data_path = '~/tmpdev0/text-to-image-gen-model-acgan/output/samples_final'
+    #data_path = '~/tmpdev0/text-to-image-gen-model-acgan/output/samples_final'
+    data_path = '/home/ooo/Documents/CS236/text-to-image-gen-model-acgan/output/samples_gen'
     eval_dataset = torchvision.datasets.ImageFolder(
         root = data_path,
         transform = transforms.Compose([
@@ -88,7 +89,7 @@ def load_dataset(batch_size):
 
 if __name__ == '__main__':
 
-    batch_size = 10
+    batch_size = 50
     eval_loader = load_dataset(batch_size)
 
     print ("Calculating Inception Score...")

train_acgan.py
@@ -189,12 +189,16 @@ if opt.netG != '':
     netG.load_state_dict(torch.load(opt.netG))
 #print(netG)
 
+# if using cuda
+if opt.cuda:
+    netG.cuda()
+
 encoder = BERTEncoder()
 
 if opt.sample == 'noshuffle':
     print('sampling images based on fixed sequence of categories ...')
 
-    sample_batch_size = 10
+    sample_batch_size = 50
     sample_dataloader = torch.utils.data.DataLoader(
         val_dataset,
         batch_size=sample_batch_size,
@@ -202,7 +206,8 @@ if opt.sample == 'noshuffle':
         num_workers=int(opt.workers),
     )
 
-    sample_final_image(netG, encoder, 100, sample_batch_size, sample_dataloader, opt)
+    sample_image(netG, encoder, sample_batch_size, 5, 0, sample_dataloader, opt)
+    #sample_final_image(netG, encoder, 100, sample_batch_size, sample_dataloader, opt)
     exit(0)
 elif opt.sample == 'shuffle':
     print('sampling images based on shuffled testsets ...')
@@ -213,7 +218,7 @@ elif opt.sample == 'shuffle':
     elif opt.dataset == 'cifar10':
         eval_dataset = train_dataset
 
-    sample_batch_size = 10
+    sample_batch_size = 100
     sample_dataloader = torch.utils.data.DataLoader(
         eval_dataset,
         batch_size=sample_batch_size,
@@ -221,7 +226,7 @@ elif opt.sample == 'shuffle':
         num_workers=int(opt.workers),
     )
 
-    sample_image2(netG, encoder, 100, sample_batch_size, sample_dataloader, opt)
+    sample_image2(netG, encoder, 2000, sample_batch_size, sample_dataloader, opt)
     exit(0)
 elif opt.sample == 'none':
     # no-op
@@ -263,7 +268,6 @@ fake_label = 0
 # if using cuda
 if opt.cuda:
     netD.cuda()
-    netG.cuda()
     dis_criterion.cuda()
     aux_criterion.cuda()
     input, dis_label, aux_label = input.cuda(), dis_label.cuda(), aux_label.cuda()